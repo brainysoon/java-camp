b01bf20df69bee648792a51a42307f128a5804d0
brainysoon <brainysoon@gmail.com>
2018-03-27 21:07:34 +0800

    [Section1->PracticeC] Pick elements that contain in the list of key value of collection2 from collection1.

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -2,10 +2,16 @@ package cn.school.thoughtworks.section1;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
-    List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+
+    private static final String VALUE = "value";
+
+    List<String> collectSameElements(List<String> collection1, Map<String, List<String>> collection2) {
+
+        return collection1.stream()
+                .filter(collection2.get(VALUE)::contains)
+                .collect(Collectors.toList());
     }
 }
