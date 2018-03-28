b125089ed03ffb862ada93d816f6f128cce45302
brainysoon <brainysoon@gmail.com>
2018-03-28 21:52:15 +0800

    [Section3->PracticeC] Decrease the count that it's key contain in the list of property value of object2 by the division of 3 from collection3

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -5,10 +5,19 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PracticeC {
+
+    private static final String VALUE = "value";
+
     Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
 
         Map<String, Integer> collection3 = collectionA.stream().collect(Collectors.groupingBy(String::toString,
                 Collectors.reducing(0, item -> 1, Integer::sum)));
-        return null;
+
+        object.get(VALUE).forEach(item -> collection3.forEach((key, value) -> {
+            if (key.equals(item)) {
+                collection3.put(key, value - value / 3);
+            }
+        }));
+        return collection3;
     }
 }
