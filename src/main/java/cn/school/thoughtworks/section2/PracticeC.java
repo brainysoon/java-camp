d4266027a15c4552b4804795e8739dc6b666a8d3
brainysoon <brainysoon@gmail.com>
2018-03-28 21:25:00 +0800

    [Section2->PracticeC] Count same elements with separator and number from collection1 to a map collection and should plus the number behind the separator to sum.

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -2,11 +2,33 @@ package cn.school.thoughtworks.section2;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
-    Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
 
-        return null;
+    private static final List<String> NUMBER_SEPARATOR_CHAIN = Arrays.asList("-", "[", ":");
+    private static final String SQUARE_BRACKET_END = "]";
+
+    Map<String, Integer> countSameElements(List<String> collection1) {
+
+        return collection1.stream().collect(Collectors.groupingBy(item -> {
+                    for (String separator : NUMBER_SEPARATOR_CHAIN) {
+                        if (item.contains(separator)) {
+                            int keyEndIndex = item.indexOf(separator);
+                            return item.substring(0, keyEndIndex);
+                        }
+                    }
+                    return item;
+                },
+                Collectors.reducing(0, item -> {
+                    for (String separator : NUMBER_SEPARATOR_CHAIN) {
+                        if (item.contains(separator)) {
+                            int numberBeginIndex = item.indexOf(separator);
+                            int numberEndIndex = item.contains(SQUARE_BRACKET_END) ? item.length() - 1 : item.length();
+                            return Integer.parseInt(item.substring(numberBeginIndex + 1, numberEndIndex));
+                        }
+                    }
+                    return 1;
+                }, Integer::sum)));
     }
 }
