55b612493919a87cb4c85a6ebed52fab1f3e9174
brainysoon <brainysoon@gmail.com>
2018-03-28 21:50:10 +0800

    [Section3->PracticeC] Count value number to collection3 from collection1.

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -2,11 +2,13 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
 
+        Map<String, Integer> collection3 = collectionA.stream().collect(Collectors.groupingBy(String::toString,
+                Collectors.reducing(0, item -> 1, Integer::sum)));
         return null;
     }
 }
