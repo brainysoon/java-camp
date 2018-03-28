09e19d6c6fcb3652c8b9ba2a31471eacbdd20144
brainysoon <brainysoon@gmail.com>
2018-03-28 21:55:44 +0800

    [Section3->PracticeD] Count same elements with dash-number from collectionA to a map collection3 and should plus the number behind dash to sum.

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -2,11 +2,22 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeD {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
 
+    private final String NUMBER_SEPARATOR = "-";
+
+    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
+
+        Map<String, Integer> collection3 = collectionA.stream()
+                .collect(Collectors.groupingBy(item -> item.split(NUMBER_SEPARATOR)[0],
+                        Collectors.reducing(0, item -> {
+                            if (item.contains(NUMBER_SEPARATOR)) {
+                                return Integer.parseInt(item.split(NUMBER_SEPARATOR)[1]);
+                            }
+                            return 1;
+                        }, Integer::sum)));
         return null;
     }
 }
